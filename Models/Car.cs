c36f0ec2da96db7633861c4d124bf8fa0eb0fd41
jakechaley <jakechaley@gmail.com>
2022-02-20 16:06:55 -0800

    dry up code

--- Models/Car.cs
+++ Models/Car.cs
@@ -4,40 +4,46 @@ namespace Dealership.Models
 {
   public class Car
   {
-    private string _makeModel;
-    private int _price;
-    private int _miles;
+
+    public string MakeModel { get; set; }
+    private int Price { get; set; }
+    private int Miles { get; set; }
 
     public void SetPrice(int newPrice)
     {
-      _price = newPrice;
+      Price = newPrice;
+    }
+
+    public static string MakeSound(string sound)
+    {
+      return "Our cars sound like " + sound;
     }
 
     public Car(string makeModel, int price, int miles)
     {
-      _makeModel = makeModel;
-      _price = price;
-      _miles = miles;
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
     }
 
     public string GetMakeModel()
     {
-      return _makeModel;
+      return MakeModel;
     }
 
     public int GetPrice()
     {
-      return _price;
+      return Price;
     }
 
     public int GetMiles()
     {
-      return _miles;
+      return Miles;
     }
 
     public bool WorthBuying(int maxPrice)
     {
-      return (_price <= maxPrice);
+      return (Price <= maxPrice);
     }
   }
 }
